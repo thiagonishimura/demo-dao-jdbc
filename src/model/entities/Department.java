724abefd36512e180213873366f7095c84335d4a
Thiago Nishimura <thiagonishimura@yahoo.com.br>
2019-09-24 18:14:15 -0300

    DepartmentDao implementation and test

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -2,25 +2,26 @@ package model.entities;
 
 import java.io.Serializable;
 
-public class Department implements Serializable{
+public class Department implements Serializable {
+
 	private static final long serialVersionUID = 1L;
-	
+
 	private Integer id;
 	private String name;
 	
-	public Department( ) {
+	public Department() {
 	}
 
-	public Department(int id, String name) {
+	public Department(Integer id, String name) {
 		this.id = id;
 		this.name = name;
 	}
 
-	public int getId() {
+	public Integer getId() {
 		return id;
 	}
 
-	public void setId(int id) {
+	public void setId(Integer id) {
 		this.id = id;
 	}
 
@@ -36,7 +37,7 @@ public class Department implements Serializable{
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + id;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
 
@@ -49,7 +50,10 @@ public class Department implements Serializable{
 		if (getClass() != obj.getClass())
 			return false;
 		Department other = (Department) obj;
-		if (id != other.id)
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
 			return false;
 		return true;
 	}
@@ -57,5 +61,5 @@ public class Department implements Serializable{
 	@Override
 	public String toString() {
 		return "Department [id=" + id + ", name=" + name + "]";
-	}	
+	}
 }
